217cc914413eb3be08137189a5cbec3213215071
Evgenii <kulemin.e.a@gmail.com>
2018-10-05 12:09:17 +0300

    Migration and Edit user profile
    Added scripts for migration
    Added logic for work with user profile

--- src/main/java/com/limb/configurations/WebSecurityConfig.java
+++ src/main/java/com/limb/configurations/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.limb.configurations;
 
 import com.limb.domain.queries.SqlQueries;
 import com.limb.services.UserService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
--- src/main/java/com/limb/controllers/UserController.java
+++ src/main/java/com/limb/controllers/UserController.java
@@ -2,62 +2,62 @@ package com.limb.controllers;
 
 import com.limb.domain.Roles;
 import com.limb.domain.User;
-import com.limb.repos.UserRepo;
+import com.limb.services.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
-    private final UserRepo userRepo;
+    private final UserService userService;
 
-    public UserController(UserRepo userRepo) {
-        this.userRepo = userRepo;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
-    public String userList(Model model){
-        model.addAttribute("users", userRepo.findAll());
+    public String userList(Model model) {
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
-    public String userEditForm(@PathVariable User user, Model model){
+    public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Roles.values());
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userID") User user,
-            Model model){
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Roles.values())
-                .map(Roles::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for(String key : form.keySet()){
-            if(roles.contains(key)){
-                user.getRoles().add(Roles.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+            Model model) {
+        userService.saveUser(user, username, form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password){
+
+        userService.updateProfile(user, password);
+        return "redirect:/user/profile";
     }
 }
